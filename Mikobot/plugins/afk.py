b9e27d99958f50a7334be8bbe2329cf1db39f761
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-02-03 10:57:52 +0530

    Update afk.py

--- Mikobot/plugins/afk.py
+++ Mikobot/plugins/afk.py
@@ -44,12 +44,12 @@ async def afk(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         if reason:
             await update.effective_message.reply_text(
-                f"➲ {fname} is now away! \n\n➦ Reason: <code>{reason}</code> \n {notice}",
+                f"➲ {fname} ɪs ɴᴏᴡ ᴀᴡᴀʏ! \n\n➦ ʀᴇᴀsᴏɴ: <code>{reason}</code> \n {notice}",
                 parse_mode="html",
             )
         else:
             await update.effective_message.reply_text(
-                "➲ {} is now away!{}".format(fname, notice),
+                "➲ {} ɪs ɴᴏᴡ ᴀᴡᴀʏ!{}".format(fname, notice),
             )
     except BadRequest:
         pass
@@ -74,18 +74,18 @@ async def no_longer_afk(update: Update, context: ContextTypes.DEFAULT_TYPE):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "➲ {} is here!",
-                "➲ {} is back!",
-                "➲ {} is now in the chat!",
-                "➲ {} is awake!",
-                "➲ {} is back online!",
-                "➲ {} is finally here!",
-                "➲ Welcome back! {}",
+                "➲ {} ɪs ʜᴇʀᴇ!",
+                "➲ {} is ʙᴀᴄᴋ!",
+                "➲ {} ɪs ɴᴏᴡ ɪɴ ᴛʜᴇ ᴄʜᴀᴛ!",
+                "➲ {} ɪs ᴀᴡᴀᴋᴇ!",
+                "➲ {} ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ!",
+                "➲ {} ɪs ғɪɴᴀʟʟʏ ʜᴇʀᴇ!",
+                "➲ ᴡᴇʟᴄᴏᴍᴇ ʙᴀᴄᴋ! {}",
             ]
             chosen_option = random.choice(options)
             await update.effective_message.reply_text(
                 chosen_option.format(firstname)
-                + f"\n\nYou were AFK for: <code>{time}</code>",
+                + f"\n\nʏᴏᴜ ᴡᴇʀᴇ ᴀғᴋ ғᴏʀ: <code>{time}</code>",
                 parse_mode="html",
             )
         except:
@@ -204,7 +204,7 @@ function(AFK_REGEX_HANDLER, AFK_GROUP)
 function(NO_AFK_HANDLER, AFK_GROUP)
 function(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK"
+__mod_name__ = "ᴀғᴋ"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
