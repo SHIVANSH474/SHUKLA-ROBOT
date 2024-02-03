629cdbac9666c77f2892660e9c8de96750eea8d7
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-02-03 11:43:06 +0530

    Update karma.py

--- Mikobot/plugins/karma.py
+++ Mikobot/plugins/karma.py
@@ -165,7 +165,7 @@ async def karma_toggle_xd(_, message):
 # <=================================================== HELP ====================================================>
 
 
-__mod_name__ = "KARMA"
+__mod_name__ = "ᴋᴀʀᴍᴀ"
 __help__ = """
 
 ➠ *UPVOTE* - Use upvote keywords like "+", "+1", "thanks", etc. to upvote a message.
